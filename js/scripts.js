77d3a93a5f3106bdbf95a708981e7ad2ae78c139
eileen <eileen@eileen.com>
2017-05-10 16:52:33 +0200

    removing comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,3 @@
-// choinka krzywa
-/*
-function drawTree(h) {
-	for (var i = 1; i <= h; i++) {
-		var star = '';
-		for (var j = 1; j <= i; j++) {
-			star  += '*';
-		};
-		console.log(star);
-	}
-
-};
-
-drawTree(7);
-*/
-// choinka prosta
-
 function drawTree(h) {
 	for (var i = 1; i <= h; i++) {
 		var space = '';
